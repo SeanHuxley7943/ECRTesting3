617e246bb26fe3e34629a0ad134a61c9323934fd
Swastik Purathepparambil <swastiksp25@gmail.com>
2023-01-04 21:01:46 -0800

    is aws.yml working?

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.handler = (event, context) => {
-    string1 = "Hello, My name is Swastik Purathepparambil.";
-    string2 = "If I am correct, this should be working now."
+    string1 = "Hello, My name is Swastik Purathepparambil!";
+    string2 = "If I am correct, this should be working now!"
     return (string1 + " " + string2);
 };
