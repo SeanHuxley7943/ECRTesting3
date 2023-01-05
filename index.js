d6e443a5301610737a8ba305d69ac89906c04b90
Swastik Purathepparambil <swastiksp25@gmail.com>
2023-01-04 21:19:12 -0800

    fully working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 exports.handler = (event, context) => {
     string1 = "Hello, My name is Swastik Purathepparambil!";
     string2 = "If I am correct, this should be working now!"
-    return (string1 + " " + string2);
+    string3 = "Yes It is working fully!"
+    return (string1 + " " + string2 + " " + string3);
 };
