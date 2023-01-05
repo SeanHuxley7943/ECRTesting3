a38d0493afe25fcd8a8e2df84b205eb782026ce9
Swastik Purathepparambil <swastiksp25@gmail.com>
2023-01-04 20:45:39 -0800

    Checking If ECR works

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 exports.handler = (event, context) => {
-    return "Index says Ah Yes it should be working now hopefully!";
+    string1 = "Hello, My name is Swastik Purathepparambil.";
+    string2 = "If I am correct, this should be working now."
+    return (string1 + " " + string2);
 };
